03cec9994b208c134a5c57e05a2924a33c7c6883
Aleks <alex.marinic22@gmail.com>
2022-09-18 21:27:57 +0200

    Add ERK presentation with bigger fonts.

---
+++ erk22/assets/DrawingControls.17fefb7e.js
@@ -0,0 +1,1 @@
+import"./DrawingControls.vue_vue_type_script_setup_true_lang.0b9f084e.js";import{_ as t}from"./DrawingControls.vue_vue_type_script_setup_true_lang.0b9f084e.js";import"./index.47490e47.js";export{t as default};
---
+++ erk22/assets/DrawingPreview.ea2f7a95.js
@@ -0,0 +1,1 @@
+import"./index.47490e47.js";import{ai as o}from"./index.47490e47.js";export{o as default};
